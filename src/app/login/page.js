f20f56db6874e9d65888e2425f6d5b269ff8e88d
anjmhrjn <anjmhrjn1@gmail.com>
2025-07-15 23:16:10 -0400

    login register page ui

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "../../context/AuthContext";
 import { toast } from "react-toastify";
 import RedirectIfAuth from "@/components/RedirectIfAuth";
+import Link from "next/link";
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
@@ -29,28 +30,82 @@ export default function LoginPage() {
 
   return (
     <RedirectIfAuth>
-      <form onSubmit={handleLogin} className="p-4 max-w-md mx-auto">
-        <h1 className="text-2xl mb-4">Login</h1>
-        <input
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="border p-2 mb-2 w-full"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="border p-2 mb-4 w-full"
-        />
-        <button
-          type="submit"
-          className="bg-blue-600 text-white px-4 py-2 rounded"
-        >
-          Login
-        </button>
-      </form>
+      <div className="flex min-h-full justify-center px-6 py-12 lg:px-8">
+        <div className="shadow-xl rounded-2xl md:w-1/3">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+              Sign in to your account
+            </h2>
+          </div>
+
+          <div className="my-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" onSubmit={handleLogin}>
+              <div>
+                <label
+                  for="username"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autocomplete="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    for="password"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    autocomplete="current-password"
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm/6 text-gray-500">
+              Not a member?
+              <Link
+                href="/register"
+                className="font-semibold text-indigo-600 hover:text-indigo-500"
+              >
+                Register
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </RedirectIfAuth>
   );
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "../../context/AuthContext";
 import { toast } from "react-toastify";
 import RedirectIfAuth from "@/components/RedirectIfAuth";
+import Link from "next/link";
 
 export default function LoginPage() {
   const [username, setUsername] = useState("");
@@ -60,7 +61,7 @@ export default function LoginPage() {
           toast.success("User registration successful!");
           router.push("/login");
         } else {
-            toast.error(res.message || "Error registering user")
+          toast.error(res.message || "Error registering user");
         }
       }
     } catch (err) {
@@ -70,54 +71,164 @@ export default function LoginPage() {
 
   return (
     <RedirectIfAuth>
-      <form onSubmit={handleRegister} className="p-4 max-w-md mx-auto">
-        <h1 className="text-2xl mb-4">Register</h1>
-        <input
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="border p-2 mb-2 w-full"
-        />
-        <input
-          placeholder="Email"
-          value={email}
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          className="border p-2 mb-2 w-full"
-        />
-        <input
-          placeholder="First Name"
-          value={firstname}
-          onChange={(e) => setFirstname(e.target.value)}
-          className="border p-2 mb-2 w-full"
-        />
-        <input
-          placeholder="Last Name"
-          value={lastname}
-          onChange={(e) => setLastname(e.target.value)}
-          className="border p-2 mb-2 w-full"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="border p-2 mb-4 w-full"
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          className="border p-2 mb-4 w-full"
-        />
-        <button
-          type="submit"
-          className="bg-blue-600 text-white px-4 py-2 rounded"
-        >
-          Register
-        </button>
-      </form>
+      <div className="flex min-h-full justify-center px-6 py-12 lg:px-8">
+        <div className="shadow-xl rounded-2xl md:w-1/3">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+              Register your account
+            </h2>
+          </div>
+
+          <div className="my-5 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-3" onSubmit={handleRegister}>
+              <div>
+                <label
+                  for="username"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autocomplete="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  for="email"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autocomplete="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  for="firstname"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  First Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="firstname"
+                    id="firstname"
+                    autocomplete="firstname"
+                    value={firstname}
+                    onChange={(e) => setFirstname(e.target.value)}
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  for="lastname"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Last Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="lastname"
+                    autocomplete="lastname"
+                    value={lastname}
+                    onChange={(e) => setLastname(e.target.value)}
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    for="password"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    autocomplete="current-password"
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    for="confirmPassword"
+                    className="block text-sm/6 font-medium text-gray-900"
+                  >
+                    Confirm Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    autocomplete="current-password"
+                    required
+                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Register
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm/6 text-gray-500">
+              Already Registered?
+              <Link
+                href="/login"
+                className="font-semibold text-indigo-600 hover:text-indigo-500"
+              >
+                Sign In
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
     </RedirectIfAuth>
   );
 }
